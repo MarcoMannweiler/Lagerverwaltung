b144b40d5a306ca036739138a85d5736a5669b53
Fifi <fifi@MacBook-Air.fritz.box>
2024-09-22 08:56:24 +0200

    Flter und Eingaben aus der Sidebar in die Mitte

--- pages/3_Ausbuchung.py
+++ pages/3_Ausbuchung.py
@@ -13,13 +13,10 @@ df = get_dataframe(dateipfad=dateipfad, name=name)
 ######################## Produkt filtern und ausbuchen  #####################################
 ##############################      Start     ###############################################
 
+st.header("Produkt suchen und Menge x ausbuchen")
+st.subheader("Filteroptionen")
 
-# Sidebar zum Filtern
-st.sidebar.header("Produkt suchen und Menge x ausbuchen")
-st.sidebar.subheader("Filteroptionen")
-
-# Filter-Optionen zur Einschränkung der Suche
-# Zustände für die Filter speichern
+# Initialisierung der Session State Filter Variablen, falls sie noch nicht existieren
 if 'selected_brand' not in st.session_state:
     st.session_state.selected_brand = ""
 if 'selected_typ' not in st.session_state:
@@ -37,16 +34,8 @@ if 'selected_definition' not in st.session_state:
 if 'selected_psa' not in st.session_state:
     st.session_state.selected_psa = ""
 
-# Filter-Optionen zur Einschränkung der Suche
-selected_brand,selected_typ,selected_serie,selected_set,selected_language,selected_edition,selected_definition,selected_psa = make_filter_options(df=df)
-
-
-# Anwenden der Filter auf den DataFrame
-filtered_df = apply_filters(df, selected_brand, selected_typ, selected_serie, selected_set, selected_language, selected_edition, selected_definition, selected_psa)
-
-
-# Clear Filter Button
-if st.sidebar.button("Clear Filter"):
+# Clear Filter Button prüfen und Session-State-Filter zurücksetzen
+if st.button("Clear Filter"):
     st.session_state.selected_brand = ""
     st.session_state.selected_typ = ""
     st.session_state.selected_serie = ""
@@ -55,8 +44,29 @@ if st.sidebar.button("Clear Filter"):
     st.session_state.selected_edition = ""
     st.session_state.selected_definition = ""
     st.session_state.selected_psa = ""
-    # Reset filtered_df to the original DataFrame
-    filtered_df = df.copy()
+
+# Filter-Optionen in vier Spalten auf der Hauptseite
+col1, col2, col3, col4 = st.columns(4)
+
+# Die Filter-Widgets anzeigen, wobei der Zustand aus dem Session-State verwendet wird
+with col1:
+    selected_brand = st.selectbox("Marke", [""] + sorted(df["Brand"].unique()), index=0, key="selected_brand")
+    selected_serie = st.selectbox("Serie", [""] + sorted(df["Serie"].unique()), index=0, key="selected_serie")
+
+with col2:
+    selected_typ = st.selectbox("Typ", [""] + sorted(df["Typ"].unique()), index=0, key="selected_typ")
+    selected_set = st.selectbox("Set", [""] + sorted(df["Set"].unique()), index=0, key="selected_set")
+
+with col3:
+    selected_language = st.selectbox("Sprache", [""] + sorted(df["Language"].unique()), index=0, key="selected_language")
+    selected_edition = st.selectbox("Edition", [""] + sorted(df["Edition"].unique()), index=0, key="selected_edition")
+
+with col4:
+    selected_definition = st.selectbox("Definition", [""] + sorted(df["Definition"].unique()), index=0, key="selected_definition")
+    selected_psa = st.selectbox("PSA", [""] + sorted(df["PSA"].unique()), index=0, key="selected_psa")
+
+# Anwenden der Filter auf den DataFrame
+filtered_df = apply_filters(df, selected_brand, selected_typ, selected_serie, selected_set, selected_language, selected_edition, selected_definition, selected_psa)
 
 # Anzeige des gefilterten DataFrames
 st.header("Liste des Lagerbestands")
@@ -70,20 +80,19 @@ filtered_df = filtered_df[
 
 st.dataframe(filtered_df)
 
-
 # Auswahl einer Zeile aus dem gefilterten DataFrame
 if not filtered_df.empty:
     selected_index = st.selectbox("Wählen Sie eine Zeile zum Ausbuchen aus:", filtered_df.index)
     selected_row = filtered_df.loc[selected_index]
 
     # Neue Werte für das Datum und den aktuellen Wert pro Einheit
-    new_date = st.sidebar.date_input("Neues Datum", date.today())
-    new_amount = st.sidebar.number_input("Auszubuchende Menge", value=int(selected_row["Amount"]))
-    new_current_value = st.sidebar.number_input("Verkaufspreis pro Einheit eingeben", value=float(selected_row["Current value per unit"]))
+    new_date = st.date_input("Neues Datum", date.today())
+    new_amount = st.number_input("Auszubuchende Menge", value=int(selected_row["Amount"]))
+    new_current_value = st.number_input("Verkaufspreis pro Einheit eingeben", value=float(selected_row["Current value per unit"]))
     old_amount_updated = filtered_df.loc[selected_index, 'Amount'] - new_amount
 
     # Button zum Aktualisieren
-    if st.sidebar.button("Produkt ausbuchen"):
+    if st.button("Produkt ausbuchen"):
         # Alte Zeile aktualisieren (Status auf "outdated")
         df.at[selected_index, "Status"] = f"outdated-{new_date}"
 
@@ -118,8 +127,5 @@ if not filtered_df.empty:
 st.header("Aktualisiertes Logbuch")
 st.dataframe(df)
 
-
-
-
-
-
+######################## Produkt filtern und ausbuchen  #####################################
+##############################      Ende     ###############################################
--- pages/6_Lagerbestand.py
+++ pages/6_Lagerbestand.py
@@ -1,3 +1,4 @@
+import plotly.express as px
 import streamlit as st
 import pandas as pd
 import os
@@ -9,7 +10,58 @@ dateipfad = "data/"
 name = "inventory.csv"
 df = get_dataframe(dateipfad=dateipfad, name=name)
 
-st.header("Gefilterte Inventarliste OHNE outdated Vorgänge")
-st.write("alle outdated Zeilen sind ausgeblendet, da diese nicht mehr aktualisiert oder ausgebucht werden können - bei jeder Aktualisierung oder Ausbuchung werden neue Zeilen mit current-date erstellt. Somit gibt es keine Doppelungen in den Mengen, aber alle Einbuchungen, alle Preisaktualisierungen und alle Ausbuchungen sind aufgeführt.")
+st.title('Lagerbestand nach Marken')
 
-st.dataframe(df[df['Status'].str.contains(r'^current', case=False, na=False)])
+# Kombinieren der Spalten in eine neue Spalte für den Plot
+df['Combination'] = df[['Brand', 'Typ', 'Serie', 'Set', 'Language', 'Edition', 'Definition', 'PSA']].agg('-'.join, axis=1)
+
+# Aggregiere die Amounts nach den eindeutigen Kombinationen
+agg_df = df.groupby('Combination', as_index=False)['Amount'].sum()
+
+
+# Erstellen eines vertikalen Balkendiagramms mit Plotly
+fig = px.bar(agg_df,
+             x='Combination',
+             y='Amount',
+             title='',
+             labels={'Amount': 'Amount', 'Combination': 'Kombination'},
+             color='Amount')
+
+# Plotly-Diagramm anzeigen
+st.plotly_chart(fig)
+
+
+
+st.title('Lagerbestand einer jeweiligen Marke')
+
+# Dropdown zur Auswahl der Brand-Spalte
+selected_brand = st.selectbox('Wähle eine Brand aus', df['Brand'].unique())
+
+# Daten für die ausgewählte Brand filtern
+filtered_df = df[df['Brand'] == selected_brand]
+
+# Erstellen eines horizontalen Balkendiagramms mit Plotly
+fig = px.bar(filtered_df,
+             x='Amount',
+             y='Serie',
+             orientation='h',  # Horizontal ausgerichtet
+             title=f'Amount nach Serie für {selected_brand}',
+             labels={'Amount': 'Amount', 'Serie': 'Serie'})
+
+# Plotly-Diagramm anzeigen
+st.plotly_chart(fig)
+
+
+
+
+st.header("Liste des Lagerbestands")
+st.write("Alle outdated Zeilen (1), alle Ausbuchungen (2) und alle Zeilen mit Menge 0 (3) sind ausgeblendet, da diese nicht mehr ausgebucht werden können - sie sind nicht auf Lager")
+
+filtered_df = df[
+    (df['Status'].str.contains(r'^current', case=False, na=False)) &  # Status enthält 'current'
+    (df['Amount'] != 0) &  # Amount ist nicht gleich 0
+    (~df['Vorgang'].str.contains('Ausbuchung', case=False, na=False))  # Vorgang enthält nicht 'Ausbuchung'
+]
+
+st.dataframe(filtered_df)
+
